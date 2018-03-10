37bc4c54521ab9f07d9bc350da635d55940a916c
radeshshiwlochan <radesh0430@gmail.com>
2018-03-09 23:04:59 -0500

    intial commit

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require( 'express' );
+const app     = express();
+const port    = 3000;
+
+app.use(express.static( 'public' ) );
+app.set( 'view engine', 'ejs' );
+
+let homeController = require( './controllers/home.js' );
+
+app.get( '/', homeController.index );
+
+app.listen( port, () => {
+	console.log( `App Listening on port:${port}` );
+});
