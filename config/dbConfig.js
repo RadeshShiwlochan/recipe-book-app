01e2eb76e2404c68c504bcd5f4b62d509b4222db
radeshshiwlochan <radesh0430@gmail.com>
2018-03-09 23:35:14 -0500

    adds the connection to mLab

---
+++ config/dbConfig.js
@@ -0,0 +1,11 @@
+const mongodb = require( 'mongodb' );
+const uri     = process.env.URI;
+let db_connection;
+
+mongodb.MongoClient.connect( uri, ( err, db ) => {
+	if ( err )
+		console.log( "ERROR IN CONNECTION TO MLAB", err );
+	db_connection = db;
+});
+
+module.exports.db_connection = db_connection;
